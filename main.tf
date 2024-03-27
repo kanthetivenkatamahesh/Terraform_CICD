7323a8f14e5444c9bd7d30982e5bed58d4c9db08
kanthetivenkatamahesh <107744616+kanthetivenkatamahesh@users.noreply.github.com>
2024-03-27 11:43:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-009e46eef82e25fef"
+    ami = "ami-0c101f26f147fa7fd"
     instance_type = "t2.micro"
+    subnet_id = "subnet-0da392acf275d1cb5"
     tags = {
       Name = "dev-ec2"
     }
