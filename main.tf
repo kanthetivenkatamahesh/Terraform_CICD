5b3bbd9986d472980c2de51820c37283486ecfb3
kanthetivenkatamahesh <107744616+kanthetivenkatamahesh@users.noreply.github.com>
2024-03-27 11:34:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0440d3b780d96b29d"
+    ami = "ami-033a1ebf088e56e81"
     instance_type = "t2.micro"
+    subnet_id  = "subnet-0da392acf275d1cb5"
     tags = {
       Name = "dev-ec2"
     }
